f5080e9b9d6e90e3872387722c27b3fddfce6c2c
wxh <wxhmessage@live.cn>
2025-10-05 23:10:40 +0800

    配置版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.lonepheasantwarrior.volcenginetts"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10001
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
