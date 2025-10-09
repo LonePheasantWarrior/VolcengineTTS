9758a45473077cc91014d96af767b614baa14aa4
wxh <wxhmessage@live.cn>
2025-10-09 11:20:57 +0800

    将版本号升级为1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.lonepheasantwarrior.volcenginetts"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10002
-        versionName = "1.0.2"
+        versionCode = 10003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
